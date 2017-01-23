082dae36455edd64bc8519b25d3c150942b2e287
jean-christian-petetin <jeanchristianpetetin@gmail.com>
2017-01-23 10:53:14 +0100

    Correction des routes de bootstrap.

--- src/ViewBundle/Controller/ViewController.php
+++ src/ViewBundle/Controller/ViewController.php
@@ -3,16 +3,14 @@
 namespace ViewBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class ViewController extends Controller{
     /**
      * @Route("/",name="home")
-     * @Template("ViewBundle:Default:landingPage.html.twig")
      */
     public function getLandingPage() {
-        return null;
+        return $this->render('ViewBundle:Default:landingPage.html.twig');
     }
 }
